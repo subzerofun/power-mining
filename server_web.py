c1f058820af6063f164e165e722deb8e5ed60ef8
subzerofun <dietrichstein@gmx.net>
2025-01-09 21:28:16 +0100

    Update server_web.py

--- server_web.py
+++ server_web.py
@@ -343,7 +343,6 @@ def search():
             LEFT JOIN relevant_stations rs ON s.id64 = rs.system_id64
             LEFT JOIN stations st ON s.id64 = st.system_id64 AND rs.station_name = st.station_name
             WHERE ms.ring_type = ANY(%s::text[])
-            ORDER BY rs.sell_price DESC NULLS LAST, s.distance ASC
             """
             params = [rx, rx, ry, ry, rz, rz, max_dist, signal_type, signal_type, ring_types]
 
@@ -366,13 +365,14 @@ def search():
                 st.landing_pad_size, st.distance_to_arrival as station_distance,
                 st.station_type, rs.demand, rs.sell_price, st.update_time
             FROM relevant_systems s
-            JOIN mineral_signals ms ON s.id64 = ms.system_id64
             """
             params = [rx, rx, ry, ry, rz, rz, max_dist, signal_type, signal_type]
 
             if ring_type_filter != 'Without Hotspots':
                 query += " JOIN mineral_signals ms ON s.id64 = ms.system_id64 AND ms.mineral_type = %s"
                 params.append(signal_type)
+            else:
+                query += " JOIN mineral_signals ms ON s.id64 = ms.system_id64"
 
             query += """
             LEFT JOIN relevant_stations rs ON s.id64 = rs.system_id64
@@ -596,12 +596,12 @@ def search_highest():
         
         # Get the list of non-hotspot materials
         non_hotspot = get_non_hotspot_materials_list()
-        non_hotspot_str = "'" + "','".join(non_hotspot) + "'"
+        non_hotspot_str = ','.join([f"'{mat}'" for mat in non_hotspot])
         
         # Build ring type case statement
         ring_type_cases = []
         for material, ring_types in mining_data.NON_HOTSPOT_MATERIALS.items():
-            ring_types_str = "'" + "','".join(ring_types) + "'"
+            ring_types_str = ','.join([f"'{rt}'" for rt in ring_types])
             ring_type_cases.append(f"WHEN hp.commodity_name = '{material}' AND ms.ring_type IN ({ring_types_str}) THEN 1")
         ring_type_case = '\n'.join(ring_type_cases)
         
